e1559b1185f01f4aeda7715cb323327a7ed61f2f
OLIVER RUBIO <2119100629@soy.utj.edu.mx>
2022-12-18 13:00:08 -0600

    resolved error landing

--- src/components/blocks/landing/QNav.vue
+++ src/components/blocks/landing/QNav.vue
@@ -4,7 +4,7 @@
         <a class="flex-1  justify-center hidden md:flex md:first-letter:justify-start" href="/">
             <img class="w-40" src="@/assets/logo/quahr-logo-blanco.png" alt="">
         </a>
-        <div class="flex gap-5 flex-4 justify-center">
+        <div class="flex gap-5 flex-4 justify-center flex-wrap">
             <a class="text-white text-md font-semibold" href="#feature">Características</a>
             <a class="text-white text-md font-semibold" href="#benefits">Beneficios</a>
             <a class="text-white text-md font-semibold" href="#package">Paquetes</a>
